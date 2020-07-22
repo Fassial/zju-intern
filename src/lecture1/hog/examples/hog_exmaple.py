908f5c83da06dc0dd1123fa837b9b908eeb7f1fb
fassial <2508440340@qq.com>
2020-07-22 11:52:20 +0800

    src: add hog in lecture1, build pass

---
+++ src/lecture1/hog/examples/hog_exmaple.py
@@ -0,0 +1,25 @@
+# get image
+from skimage import io
+# local dep
+import sys
+sys.path.append("../")
+from hog import hog
+
+EXAMPLE_FILE = "./example.jpg"
+RESULT_FILE = "./example-hog.jpg"
+
+"""
+test:
+    test hog
+    @params:
+        None
+    @rets:
+        None
+"""
+def test():
+    image = io.imread(EXAMPLE_FILE, as_gray = True)
+    normalised_blocks, hog_image = hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(8, 8), visualize=True)
+    io.imsave(RESULT_FILE, hog_image)
+
+if __name__ == "__main__":
+    test()
